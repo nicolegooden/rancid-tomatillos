76a178fd5575d5a73bac66856a016e796ac99caa
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-06 16:31:39 -0600

    Add componentDidMount() to make fetch request for all movies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import './tomato.jpg';
+import { getMovies } from './apiCalls';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      movies: [],
+      err: ''
+    };
+  }
+
+  componentDidMount() {
+    getMovies()
+    .then(movies => this.setState({ movies: movies }))
+    .catch(err => {
+      this.setState({  err: 'Sorry, try again later.'});
+    })
   }
 
   render() {
