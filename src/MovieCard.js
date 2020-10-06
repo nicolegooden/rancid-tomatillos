a7741ef89bee2d7b4453c5e931ec363866b01c91
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-06 15:32:38 -0600

    Create MovieCard functional component with elements

---
+++ src/MovieCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MovieCard(props) {
+  return (
+    <article className='movie-card'>
+      <img className='movie-card-image' alt='single movie card for {props.title}' src={props.posterPath} />
+      <h3 className='movie-title'>{ props.title }</h3>
+      <p className='movie-release-date'>{ props.releaseDate }</p>
+      <p className='movie-average-rating'>{ props.averageRating }</p>
+    </article>
+  )
+}
+
+export default MovieCard;
