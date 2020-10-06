09250ebf7669c42328a4a88e9d41eb6feddbc0e6
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-06 17:38:21 -0600

    Return a MovieCard for each movie in allMovies array

--- src/MovieContainer.js
+++ src/MovieContainer.js
@@ -2,9 +2,14 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 function MovieContainer(props) {
-  return (
-
-  );
+  return props.allMovies.map(movie => {
+    return <MovieCard 
+      posterPath={movie.poster_path}
+      title={movie.title}
+      releaseDate={movie.release_date}
+      averageRating={movie.average_rating}
+    />
+  })
 }
 
 export default MovieContainer;
