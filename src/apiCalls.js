77e593c2f1a1d04f98a6b37ce055bc06e5993bad
Naomi Ware <nware1066@yahoo.com>
2020-10-07 17:41:17 -0600

    Create getUserData() to post email and password

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,3 +6,20 @@
             alert('Oops, no movies to show right now!');
         })
     }
+
+    export const getUserData = (emailValue, passwordValue) => {
+      return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: emailValue,
+          password: passwordValue
+        })
+      })
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+    }
+//
