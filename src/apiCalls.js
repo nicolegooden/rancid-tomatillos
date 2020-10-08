03ef6efeb26a8f0090e2bb48f98e238662555c2e
Naomi Ware <nware1066@yahoo.com>
2020-10-07 18:20:15 -0600

    Add getUserData function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -19,7 +19,7 @@
         })
       })
       .then(response => response.json())
-      .then(response => console.log(response))
+      // .then(response => console.log(response))
       .catch(err => console.log(err))
     }
 //
