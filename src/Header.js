457285922e1713bb4f6348f6614b6e4b7747429b
Naomi Ware <nware1066@yahoo.com>
2020-10-09 17:05:09 -0600

    Add conditional logic to header text and login/logout button

--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,25 @@ import { BrowserRouter as Route, Link } from 'react-router-dom';
 import './tomato.jpg';
 import './Header.css';
 
-function Header() {
+function Header(props) {
 
-  let logButton = <Link to="/login"><button className='log-button'>Log</button></Link>
+  let logButtonText;
+  let headerText;
 
+  if (props.user.name) {
+    headerText = `Welcome To Rancid Tomatillos, ${props.user.name}`
+    logButtonText = 'Logout'
+  } else {
+    headerText = 'Rancid Tomatillos'
+    logButtonText = 'Login'
+  }
+
+  let logButton = <Link to="/login"><button className='log-button'>{logButtonText}</button></Link>
+  
   return (
     <section className='header'>
       <img className='tomato-image' src='./tomato.jpg' alt='basic tomato' />
-      <h1 className='welcome-banner'>Rancid Tomatillos</h1>
+      <h1 className='welcome-banner'>{headerText}</h1>
         {logButton}
     </section>
   )
