c65aa3a295851a11a7c649b127fad038b17523f7
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-10 11:05:46 -0600

    Create directories for each component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Route, Link } from 'react-router-dom';
 import './tomato.jpg';
 import './Header.css';
