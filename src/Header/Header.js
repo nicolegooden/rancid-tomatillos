320bfb013c1f7c958271a645f918217865390636
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-10 12:44:58 -0600

    Add aria label of 1 to h1 element

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -8,7 +8,7 @@ function Header(props) {
   return (
     <section className='header'>
       <img className='tomato-image' src='./tomato.jpg' alt='basic tomato' />
-      <h1 className='welcome-banner'>{props.determineHeaderText()}</h1>
+      <h1 aria-label='1' className='welcome-banner'>{props.determineHeaderText()}</h1>
         {props.determineLogButtonStatus()}
     </section>
   )
