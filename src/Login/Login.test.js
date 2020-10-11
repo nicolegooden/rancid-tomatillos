415e03d56fa81798e9ed38cdb049a0c9b036a900
Naomi Ware <nware1066@yahoo.com>
2020-10-11 09:35:34 -0600

    Add test for upDateLogin

--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -6,43 +6,39 @@ import Login from './Login';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Login', () => {
-//Tests:
-    //handleInputChange(event)
-    //handleLogin is fired (userEvent) when 
-        //submit button is clicked
-    //can we/should we test componentDidMount?
+
     it('should render a login form', () => {
-        // const value1 = 'charlie@turing.io'
-        // const value2 = 'qwerty'
         const fakeUpdateLogin = jest.fn();
-        const fakeHandleChange = jest.fn();
-        const handleLogin = jest.fn();
-        const myUser = {}; 
+        const fakeSetUser = jest.fn();
+        const myUser = {};
 
       render(
         <MemoryRouter>
-          <Login 
+          <Login
             user={myUser}
             updateLoginView={fakeUpdateLogin}
-            // handleInputChange={fakeHandleChange}
+            setUser={fakeSetUser}
           />
-        </MemoryRouter> 
+        </MemoryRouter>
       )
+
+      expect(fakeUpdateLogin).toHaveBeenCalledTimes(1);
 
       userEvent.type(screen.getByPlaceholderText('example@email.com'), 'charlie@turing.io');
       userEvent.type(screen.getByPlaceholderText('enter password'), 'qwerty');
-      
+
       expect(screen.getByPlaceholderText('example@email.com')).toBeInTheDocument();
       expect(screen.getByPlaceholderText('enter password')).toBeInTheDocument();
       expect(screen.getByPlaceholderText('example@email.com')).toHaveValue('charlie@turing.io');
       expect(screen.getByPlaceholderText('enter password')).toHaveValue('qwerty');
       expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
-      
+
       userEvent.click(screen.getByText('Login'));
       screen.debug();
+      // expect(fakeSetUser).toHaveBeenCalledTimes(1);
+      // Jest shows this being called zero times.. can this be tested here?
       expect(screen.getByPlaceholderText('example@email.com')).toHaveValue('');
       expect(screen.getByPlaceholderText('enter password')).toHaveValue('');
 
-    //   expect(handleLogin).toHaveBeenCalledTimes(1);
     })
 })
