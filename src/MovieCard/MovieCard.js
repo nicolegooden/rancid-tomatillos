1eb20427eb0d9404427ac541dcf0320126b5a329
Naomi Ware <nware1066@yahoo.com>
2020-10-11 12:49:15 -0600

    Add determineSingleMovie to onCLick event

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './MovieCard.css';
 
 function MovieCard(props) {
-
-
-
+  console.log(props)
+  const path = `/movies/${props.id}`;
   return (
     <article className='movie-card'>
       <img className='movie-card-image' alt='single movie card for {props.title}' src={props.posterPath} />
       <h3 className='movie-title'>{ props.title }</h3>
       <p className='movie-average-rating'>Average Rating: { props.averageRating }</p>
-      <button className='show-page-button' onClick={this.goToShowPage}>Show Me More!</button>
+      <Link to='{path}'><button className='show-page-button' onClick={props.determineSingleMovie}>Show Me More!</button></Link>
     </article>
   )
 }
