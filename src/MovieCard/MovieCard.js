b59a1182199bd8bbeef942697927a6450ca86036
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-13 16:48:07 -0600

    RMove link to ShowPage from button to image

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -6,7 +6,7 @@ function MovieCard(props) {
   const path = `/movie/${props.id}`;
   return (
     <article className='movie-card'>
-      <img className='movie-card-image' alt='single movie card for {props.title}' src={props.posterPath} />
+      <Link to={path}><img className='movie-card-image' alt='single movie card for {props.title}' src={props.posterPath} /></Link>
       <h3 className='movie-title'>{ props.title }</h3>
       <p className='movie-average-rating'>Average Rating: { Math.floor(props.averageRating) }</p>
       {props.user.name &&
@@ -14,7 +14,6 @@ function MovieCard(props) {
        <label htmlFor='Rate Movie'>Rate Movie: </label>
        <input tabIndex='0' type='number' min='1' max='10' className='user-rating-input' placeholder='rate me'/><br />
        <button className='submit-rating-button'>Submit</button><br />
-       <Link to={path}><button className='show-page-button'>Movie Details</button></Link>
       </>
       }
     </article>
