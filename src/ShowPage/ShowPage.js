f9016e63087139573da7ecf697877da90a5fa466
Naomi Ware <nware1066@yahoo.com>
2020-10-12 18:28:27 -0600

    Add conditional logic to render

--- src/ShowPage/ShowPage.js
+++ src/ShowPage/ShowPage.js
@@ -10,7 +10,7 @@ import './ShowPage.css';
 
 class ShowPage extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       currentMovie: {},
       error: ''
@@ -35,24 +35,34 @@ class ShowPage extends Component {
     }
 
     render() {
+      let userView;
+      console.log(this.state.currentMovie)
+      if (this.state.currentMovie.overview) {
+        userView = <section className='show-page-container' >
+                      <article className='show-page-basic-information'>
+                        <h2 className='show-page-title'>{this.props.title}title</h2>
+                        <img className='show-page-image' alt='poster for {this.props.title}' src={ this.props.backdropPath }/>
+                        <p className='tagline'>tagline</p>
+                        <p className='run-time'>runtime</p>
+                        <p className='show-page-average-rating'>{this.props.averageRating}</p>
+                        <p className='user-rating'>{this.props.userRating}</p>
+                      </article>
+                      <article className='show-page-additional-information'>
+                        <p className='genres'>{this.getGenres}</p>
+                        <p className='overview'>{this.state.currentMovie.overview}</p>
+                        <p className='release-date'>{this.props.releaseDate}</p>
+                        <p className='budget'>{this.props.budget}</p>
+                        <p className='revenue'>{this.props.revenue}</p>
+                      </article>
+                    </section>
+      } else {
+        userView = <h1>Loading your movie</h1>
+      }
+      
       return (
-          <section className='show-page-container' >
-            <article className='show-page-basic-information'>
-              <h2 className='show-page-title'>{this.props.title}title</h2>
-              <img className='show-page-image' alt='poster for {this.props.title}' src={ this.props.backdropPath }/>
-              <p className='tagline'>tagline</p>
-              <p className='run-time'>runtime</p>
-              <p className='show-page-average-rating'>{this.props.averageRating}</p>
-              <p className='user-rating'>{this.props.userRating}</p>
-            </article>
-            <article className='show-page-additional-information'>
-              <p className='genres'>{this.getGenres}</p>
-              <p className='overview'>{this.state.currentMovie.overview}</p>
-              <p className='release-date'>{this.props.releaseDate}</p>
-              <p className='budget'>{this.props.budget}</p>
-              <p className='revenue'>{this.props.revenue}</p>
-            </article>
-          </section>
+        <div>
+          {userView}
+        </div>
       )
     }
   }
