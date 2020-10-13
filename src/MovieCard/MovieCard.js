11409994b485db6b85f04dc15e9975137bab7f9b
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-13 17:02:31 -0600

    Make MovieCard a class coomponent

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './MovieCard.css';
 
-function MovieCard(props) {
-  const path = `/movie/${props.id}`;
-  return (
-    <article className='movie-card'>
-      <Link to={path}><img className='movie-card-image' alt='single movie card for {props.title}' src={props.posterPath} /></Link>
-      <h3 className='movie-title'>{ props.title }</h3>
-      <p className='movie-average-rating'>Average Rating: { Math.floor(props.averageRating) }</p>
-      {props.user.name &&
-      <>
-       <label htmlFor='Rate Movie'>Rate Movie: </label>
-       <input tabIndex='0' type='number' min='1' max='10' className='user-rating-input' placeholder='rate me'/><br />
-       <button className='submit-rating-button'>Submit</button><br />
-      </>
-      }
-    </article>
-  )
-}
+class MovieCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    const path = `/movie/${this.props.id}`;
+    return (
+      <article className='movie-card'>
+        <Link to={path}><img className='movie-card-image' alt='single movie card for {this.props.title}' src={this.props.posterPath} /></Link>
+        <h3 className='movie-title'>{ this.props.title }</h3>
+        <p className='movie-average-rating'>Average Rating: { Math.floor(this.props.averageRating) }</p>
+        {this.props.user.name &&
+        <>
+        <label htmlFor='Rate Movie'>Rate Movie: </label>
+        <input tabIndex='0' type='number' min='1' max='10' className='user-rating-input' placeholder='rate me'/><br />
+        <button className='submit-rating-button'>Submit</button><br />
+        </>
+        }
+      </article>
+  )}
+} 
 
 export default MovieCard;
