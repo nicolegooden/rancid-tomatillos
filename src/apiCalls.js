dc177727c86c27e0f617dde0b0d67f93775acbaf
Naomi Ware <nware1066@yahoo.com>
2020-10-14 21:10:17 -0600

    Add DELETE fetch request for userRating

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -50,5 +50,12 @@
         .catch(error => console.log(error))
     }
 
-
-
+    export const deleteRating = (userID, ratingID) => {
+        return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/users/${userID}/ratings/${ratingID}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({})
+        }).catch(error => console.log(error))
+        }
