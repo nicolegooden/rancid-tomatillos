07387aab0621ab4da5668591f659cab2365280a6
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-17 17:49:11 -0600

    Add two unit tests for error messages upon unsuccessful login

--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElement } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import Login from './Login';
 import { MemoryRouter } from 'react-router-dom';
+// import { getUserData, getAllRatings } from '../apiCalls.js';
+// jest.mock('../apiCalls.js')
 
 describe('Login', () => {
 
@@ -34,11 +36,72 @@ describe('Login', () => {
       expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
 
       userEvent.click(screen.getByText('Login'));
-      screen.debug();
       // expect(fakeSetUser).toHaveBeenCalledTimes(1);
       // Jest shows this being called zero times.. can this be tested here?
       expect(screen.getByPlaceholderText('example@email.com')).toHaveValue('');
       expect(screen.getByPlaceholderText('enter password')).toHaveValue('');
+    })
 
+    it('should inform the user when login criteria is invalid', async () => {
+      const fakeUpdateLogin = jest.fn();
+      const fakeSetUser = jest.fn();
+      const myUser = {};
+ 
+      render(
+        <MemoryRouter>
+          <Login
+            user={myUser}
+            updateLoginView={fakeUpdateLogin}
+            setUser={fakeSetUser}
+          />
+        </MemoryRouter>
+      )
+
+      userEvent.type(screen.getByPlaceholderText('example@email.com'), 'charlie@turing.io');
+      userEvent.type(screen.getByPlaceholderText('enter password'), 'letmein');
+
+      expect(screen.getByPlaceholderText('example@email.com')).toHaveValue('charlie@turing.io');
+      expect(screen.getByPlaceholderText('enter password')).toHaveValue('letmein');
+
+      userEvent.click(screen.getByText('Login'));
+
+      const message = await waitForElement(() => screen.getByText('Incorrect email or password, please try again.'));
+      expect(message).toBeInTheDocument();
+
+      userEvent.type(screen.getByPlaceholderText('example@email.com'), 'leta@turing.com');
+      userEvent.type(screen.getByPlaceholderText('enter password'), 'qwerty');
+
+      expect(screen.getByPlaceholderText('example@email.com')).toHaveValue('leta@turing.com');
+      expect(screen.getByPlaceholderText('enter password')).toHaveValue('qwerty');
+
+      // expect(fakeSetUser).toHaveBeenCalledTimes(1)
+      // getUserData.mockResolvedValueOnce({})
+      expect(message).toBeInTheDocument();
+    })
+
+    it('should inform the user when they have left email or password inputs empty', () => {
+      const fakeUpdateLogin = jest.fn();
+      const fakeSetUser = jest.fn();
+      const myUser = {};
+ 
+      render(
+        <MemoryRouter>
+          <Login
+            user={myUser}
+            updateLoginView={fakeUpdateLogin}
+            setUser={fakeSetUser}
+          />
+        </MemoryRouter>
+      )
+
+      userEvent.type(screen.getByPlaceholderText('example@email.com'), 'charlie@turing.io');
+
+      expect(screen.getByPlaceholderText('example@email.com')).toHaveValue('charlie@turing.io');
+      expect(screen.getByPlaceholderText('enter password')).toHaveValue('');
+
+      userEvent.click(screen.getByText('Login'));
+
+      const otherMessage = screen.getByText('Please enter your email and your password.')
+      expect(otherMessage).toBeInTheDocument()
     })
 })
