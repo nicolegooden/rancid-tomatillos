62017d2525e64565fc48a5f6b471e07f9919fd76
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-20 15:01:21 -0600

    Change the way text is presented in the comment card

--- src/ShowPage/ShowPage.js
+++ src/ShowPage/ShowPage.js
@@ -29,8 +29,8 @@ class ShowPage extends Component {
       return this.state.allComments.map(commentInfo => {
         return (
           <article className='comment-card'>
-            <p>Comment: {commentInfo.comment}</p>
-            <p>Author: {commentInfo.author}</p>
+            <p>"{commentInfo.comment}"</p>
+            <p> - {commentInfo.author}</p>
           </article>
         )
       })
@@ -68,36 +68,35 @@ class ShowPage extends Component {
     render() {
       let userView;
       if (this.state.currentMovie.overview) {
-        userView = <main>
-                    <h2 className='show-page-title'>{this.props.title}</h2>
-                    <section className='show-page-container' >
-                      <article className='show-page-basic-information'>
-                        <img className='show-page-image' alt='poster for {this.props.title}' src={ this.state.currentMovie.backdrop_path }/>
-                        <p className='tagline'>{this.state.currentMovie.tagline}</p>
-                        <article className='ratings'>
-                          <p className='show-page-average-rating'>Average Rating: {Math.floor(this.props.average_rating)}/10</p>
-                          {this.props.findUserRating(this.props.id) > 0 &&
-                          <p className='user-rating'>My Rating: {this.props.findUserRating(this.props.id)}/10</p>}
-                        </article>
-                      </article>
-                      <article className='show-page-additional-information'>
-                      <p className='run-time'>Runtime: {this.state.currentMovie.runtime} minutes</p>
-                        <p className='genres'>Genres: {this.getGenres()}</p>
-                        <p className='overview'>{this.state.currentMovie.overview}</p>
-                        <p className='release-date'>Release Date: {this.state.currentMovie.release_date}</p>
-                        {/* <p className='budget'>Budget: ${this.state.currentMovie.budget}</p> */}
-                        {/* <p className='revenue'>Revenue: ${this.state.currentMovie.revenue}</p> */}
-                      </article>
-                    </section>
-                    <article className='comment-form'>
-                        <label htmlFor='comment-input'>Write Review: </label>
-                        <textarea value={this.state.commentInput} onChange={this.handleChange} rows='5' cols='25' wrap='hard' className='comment-input'></textarea>
-                        <button onClick={this.submitReview}>Submit</button>
-                      </article>
-                      <section className='all-comments'>
-                        {this.showAllComments()}
-                      </section>
-                    </main>
+        userView = 
+          <main>
+            <h2 className='show-page-title'>{this.props.title}</h2>
+            <section className='show-page-container' >
+              <article className='show-page-basic-information'>
+                <img className='show-page-image' alt='poster for {this.props.title}' src={ this.state.currentMovie.backdrop_path }/>
+                <p className='tagline'>{this.state.currentMovie.tagline}</p>
+                <article className='ratings'>
+                   <p className='show-page-average-rating'>Average Rating: {Math.floor(this.props.average_rating)}/10</p>
+                   {this.props.findUserRating(this.props.id) > 0 &&
+                   <p className='user-rating'>My Rating: {this.props.findUserRating(this.props.id)}/10</p>}
+                </article>
+              </article>
+              <article className='show-page-additional-information'>
+                <p className='run-time'>Runtime: {this.state.currentMovie.runtime} minutes</p>
+                <p className='genres'>Genres: {this.getGenres()}</p>
+                <p className='overview'>{this.state.currentMovie.overview}</p>
+                <p className='release-date'>Release Date: {this.state.currentMovie.release_date}</p>
+              </article>
+            </section>
+            <article className='comment-form'>
+              <label htmlFor='comment-input'>Write Review: </label>
+              <textarea value={this.state.commentInput} onChange={this.handleChange} rows='5' cols='25' wrap='hard' className='comment-input'></textarea>
+              <button onClick={this.submitReview}>Submit</button>
+            </article>
+            <section className='all-comments'>
+              {this.showAllComments()}
+            </section>
+          </main>
       } else {
         userView = <h1>Loading your movie</h1>
       }
