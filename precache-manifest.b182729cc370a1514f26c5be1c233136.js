4ad361df72d69cdc87a60100953937f06d484c57
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-22 09:22:28 -0600

    Updates

---
+++ precache-manifest.b182729cc370a1514f26c5be1c233136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a7bc756626fec5b3db28f4c082eaed",
+    "url": "/rancid-tomatillos/index.html"
+  },
+  {
+    "revision": "6d05cbf5d8bd75f6acba",
+    "url": "/rancid-tomatillos/static/css/main.06c91129.chunk.css"
+  },
+  {
+    "revision": "80dc11461cd009d8859b",
+    "url": "/rancid-tomatillos/static/js/2.918a064f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rancid-tomatillos/static/js/2.918a064f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d05cbf5d8bd75f6acba",
+    "url": "/rancid-tomatillos/static/js/main.feaf13bb.chunk.js"
+  },
+  {
+    "revision": "2fa7391ddcac566ba8c8",
+    "url": "/rancid-tomatillos/static/js/runtime-main.6628c3a3.js"
+  },
+  {
+    "revision": "6ec1b1ac9734697a111362c414d1ab95",
+    "url": "/rancid-tomatillos/static/media/tomato.6ec1b1ac.png"
+  }
+]);
